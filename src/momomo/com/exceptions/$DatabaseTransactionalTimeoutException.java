8120a1e4dfffeef82d9a1d6d660cdd8b3d3351e1
jseifeddine@apple.com <jseifeddine@apple.com>
2024-10-25 12:03:44 +0200

    Committing Fri Oct 25 12:03:44 CEST 2024

---
+++ src/momomo/com/exceptions/$DatabaseTransactionalTimeoutException.java
@@ -0,0 +1,10 @@
+package momomo.com.exceptions;
+
+/**
+ * @author Joseph S.
+ */
+public class $DatabaseTransactionalTimeoutException extends $DatabaseException {
+    public $DatabaseTransactionalTimeoutException(Throwable ex) {
+        super(ex);
+    }
+}
